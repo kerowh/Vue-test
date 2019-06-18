754a735c547aeba9769be556eab761652a8fc76e
keo <380213949@qq.com>
2019-06-18 14:52:46 +0800

    完成按钮基本样式

---
+++ src/button.vue
@@ -0,0 +1,30 @@
+<template>
+    <button class="g-button">hi</button>
+</template>
+<script>
+    export default {}
+</script>
+<style lang="scss">
+    .g-button{
+        font-size: var(--font-size);
+        height: var(--button-height);
+        padding: 0 1em;
+        border-radius: var(--border-radius);
+        background: var(--button-bg);
+        border: 1px solid var(--border-color);
+
+    }
+
+    &:hover{
+        border-color: var(--border-color-hover);
+    }
+
+    &:active{
+        background-color: var(--button-active-bg);
+    }
+
+    &:focus{
+        outline: none;
+    }
+
+</style>

a7aa8b18c4ed295c279807515982654a1d95042d
keo <380213949@qq.com>
2019-06-19 16:36:02 +0800

    检测子元素是不是button

--- src/button-group.vue
+++ src/button-group.vue
@@ -4,7 +4,16 @@
     </div>
 </template>
 <script>
-    export default {}
+    export default {
+        mounted() {
+            for (let node of this.$el.children){
+                let name = node.nodeName.toLowerCase()
+                if (name!=='button'){
+                    console.warn(`g-button-group的子元素应该全都是g-button，而你写的是${name}`)
+                }
+            }
+        }
+    }
 </script>
 <style lang="scss">
     .g-button-group{

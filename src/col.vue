519df762bd695c26a2684f8a36ca66a6e31a558a
keo <380213949@qq.com>
2019-06-26 17:20:07 +0800

    把col和row改成加上gutter

--- src/col.vue
+++ src/col.vue
@@ -1,6 +1,9 @@
 <template>
-    <div class="col" :class="[`col-${span}`]">
-        <slot></slot>
+    <div class="col" :class="[span && `col-${span}`,offset && `offset-${offset}`]"
+    :style="{paddingLeft:gutter/2+'px',paddingRight:gutter/2+'px'}">
+        <div style="border:1px solid green;height: 100px;">
+            <slot></slot>
+        </div>
     </div>
 </template>
 <script>
@@ -9,6 +12,14 @@
         props:{
             span:{
                 type:[Number,String]
+            },
+            offset:{
+                type: [Number,String]
+            }
+        },
+        data(){
+            return{
+                gutter:0
             }
         }
     }
@@ -16,9 +27,9 @@
 <style scoped lang="scss">
     .col{
         height: 100px;
-        background: grey;
+        /*background: grey;*/
         width: 50%;
-        border: 1px solid red;
+        /*border: 1px solid red;*/
     }
 
     $class: col-;
@@ -27,6 +38,15 @@
 
         .col.#{$class}#{$n} {
             width: ($n/24)*100%;
+        }
+    }
+
+    $class: offset-;
+    //前缀
+    @for $n from 1 through 24 {
+
+        .col.#{$class}#{$n} {
+            margin-left: ($n/24)*100%;
         }
     }
 
--- src/row.vue
+++ src/row.vue
@@ -1,10 +1,31 @@
 <template>
-    <div class="row">
+    <div class="row" :style="{marginLeft:-gutter/2+'px',
+    marginRight:-gutter/2+'px'}">
         <slot></slot>
     </div>
 </template>
 <script>
-
+    export default {
+        props:{
+            gutter:{
+                type:[Number,String]
+            }
+        },
+        mounted() {
+            console.log('row mounted')
+            //this指的是谁？
+            //vm实例，row
+            console.log(this.$children);
+            this.$children.forEach((vm)=>{
+                console.log(1);
+                //这里的vm是指row的child，col
+                vm.gutter = this.gutter
+                //这里的this还是vm实例，row
+                //这样做是为了把row上的gutter
+                //传入col中
+            })
+        }
+    }
 </script>
 <style scoped lang="scss">
     .row{

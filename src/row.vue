0680de7d63438cf9695ccb73f4b70e0389858763
keo <380213949@qq.com>
2019-06-26 21:14:30 +0800

    重构row和col

--- src/row.vue
+++ src/row.vue
@@ -1,6 +1,5 @@
 <template>
-    <div class="row" :style="{marginLeft:-gutter/2+'px',
-    marginRight:-gutter/2+'px'}">
+    <div class="row" :style="rowStyle">
         <slot></slot>
     </div>
 </template>
@@ -12,18 +11,22 @@
             }
         },
         mounted() {
-            console.log('row mounted')
             //this指的是谁？
             //vm实例，row
-            console.log(this.$children);
             this.$children.forEach((vm)=>{
-                console.log(1);
                 //这里的vm是指row的child，col
                 vm.gutter = this.gutter
                 //这里的this还是vm实例，row
                 //这样做是为了把row上的gutter
                 //传入col中
             })
+        },
+        computed:{
+            rowStyle(){
+                let {gutter} = this
+                return {marginLeft:-gutter/2+'px',
+                    marginRight:-gutter/2+'px'}
+            }
         }
     }
 </script>

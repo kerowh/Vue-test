ca41bdc29678f4ee569bba9d809e01cbd49e2e0e
keo <380213949@qq.com>
2019-06-29 10:46:24 +0800

    基本组件完成

---
+++ src/sider.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="sider">
+        <slot></slot>
+    </div>
+</template>
+<script>
+    export default {
+        name:'GuLuSider'
+    }
+</script>
+<style lang="scss" scoped>
+    .sider{
+
+    }
+</style>

8d35561e0f951a000fdf6d87eea2d9c41476a9f6
keo <380213949@qq.com>
2019-07-01 11:12:08 +0800

    实现了只有一个toast的功能

--- src/plugin.js
+++ src/plugin.js
@@ -11,16 +11,35 @@
 //     }
 // }
 import Toast from './toast'
+let currentToast
 export default {
     install(Vue,options){
         Vue.prototype.$toast = function (message,toastoptions) {
-            let Constructor = Vue.extend(Toast)
-            let toast = new Constructor({
-                propsData: toastoptions
-            })
-            toast.$slots.default = [message]
-            toast.$mount()
-            document.body.appendChild(toast.$el)
+            if (currentToast){
+                currentToast.close()
+            }
+            currentToast =  createToast({Vue,message,propsDate:toastoptions})
         }
     }
+}
+
+
+
+/*help function*/
+
+
+
+function createToast({Vue,message,propsDate}){
+    let Constructor = Vue.extend(Toast)
+    let toast = new Constructor(
+        /*读传入的propsDate*/
+        // { propsDate:propsDate}
+        /*两边一样的可以简写*/
+        {propsDate}
+        // {propsData: toastoptions}
+    )
+    toast.$slots.default = [message]
+    toast.$mount()
+    document.body.appendChild(toast.$el)
+    return toast
 }

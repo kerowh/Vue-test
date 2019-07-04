43d27afb8d94a9e6510a54ac8e5ccf7fe530ffce
keo <380213949@qq.com>
2019-07-04 16:09:58 +0800

    使用表驱动编程来重构代码

--- src/popover.vue
+++ src/popover.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="popover" @click="onClick">
-<!--        因为slot不能加class，没法写css-->
+        <!--        因为slot不能加class，没法写css-->
         <div ref="contentWrapper" class="content-wrapper" v-if="visible"
-        :class="{[`position-${position}`]:true}">
-            <slot name="content" ></slot>
+             :class="{[`position-${position}`]:true}">
+            <slot name="content"></slot>
         </div>
         <span ref="triggerWrapper" style="display: inline-block">
             <slot></slot>
@@ -14,69 +14,72 @@
 <script>
     export default {
         name: "GuluPopover",
-        props:{
-            position:{
-                type:String,
-                default:'top',
-                validator(value){
-                    return ['top','bottom','left','right'].indexOf(value)>=0
+        props: {
+            position: {
+                type: String,
+                default: 'top',
+                validator(value) {
+                    return ['top', 'bottom', 'left', 'right'].indexOf(value) >= 0
                 }
             }
         },
-        data(){
-            return{visible:false}
+        data() {
+            return {visible: false}
         },
-        methods:{
-            positionContent(){
-                const {contentWrapper,triggerWrapper} = this.$refs
+        methods: {
+            positionContent() {
+                const {contentWrapper, triggerWrapper} = this.$refs
                 document.body.appendChild(contentWrapper)
-                let{left,top,height}=triggerWrapper.getBoundingClientRect()
-                if (this.position==='top'){
-                    contentWrapper.style.left=left+window.scrollX+'px'
-                    contentWrapper.style.top=top+window.scrollY+'px'
-                }else if (this.position==='bottom'){
-                    contentWrapper.style.left=left+window.scrollX+'px'
-                    contentWrapper.style.top=top+window.scrollY+'px'
-                }else if (this.position==='left'){
-                    contentWrapper.style.left=left+window.scrollX+'px'
-                    let{height:height2}=contentWrapper.getBoundingClientRect()
-                    contentWrapper.style.top=top+window.scrollY+
-                        (height-height2)/2+'px'
-                }else if (this.position==='right'){
-                    contentWrapper.style.left=left+window.scrollX+'px'
-                    let{height:height3}=contentWrapper.getBoundingClientRect()
-                    contentWrapper.style.top=top+window.scrollY+
-                        (height-height3)/2+'px'
+                const {left, top, height} = triggerWrapper.getBoundingClientRect()
+                const {height: height2} = contentWrapper.getBoundingClientRect()
+                let positions={
+                    top:{
+                        top:top + window.scrollY,
+                        left:left + window.scrollX
+                    },
+                    bottom:{
+                        top:top + window.scrollY,
+                        left:left + window.scrollX
+                    },
+                    left:{
+                        top:top + window.scrollY + (height - height2) / 2,
+                        left:left + window.scrollX
+                    },
+                    right:{
+                        top:top + window.scrollY + (height - height2) / 2,
+                        left:left + window.scrollX
+                    },
                 }
-
+                contentWrapper.style.top = positions[this.position].top  + 'px'
+                contentWrapper.style.left = positions[this.position].left + 'px'
             },
-            listenToDocument(){
-                let onClickDocunment = (event)=>{
+            listenToDocument() {
+                let onClickDocunment = (event) => {
                     /*如果它存在并且内容一致才什么都不做*/
-                    if (this.$refs.contentWrapper && this.$refs.contentWrapper.contains(event.target)){
+                    if (this.$refs.contentWrapper && this.$refs.contentWrapper.contains(event.target)) {
                         return
-                    }else {
+                    } else {
                         this.visible = false
-                        document.removeEventListener('click',onClickDocunment)
+                        document.removeEventListener('click', onClickDocunment)
                     }
                 }
-                document.addEventListener('click',onClickDocunment)
+                document.addEventListener('click', onClickDocunment)
             },
-            onShow(){
-                setTimeout(()=>{
+            onShow() {
+                setTimeout(() => {
                     this.positionContent()
                     this.listenToDocument()
-                },0)
+                }, 0)
             },
-            onClick(event){
-                if (this.$refs.triggerWrapper.contains(event.target)){
+            onClick(event) {
+                if (this.$refs.triggerWrapper.contains(event.target)) {
                     // console.log('点的是按钮');
                     this.visible = !this.visible
-                     if (this.visible === true){
-                         this.onShow()
-                     }
-                }else {
-                   return
+                    if (this.visible === true) {
+                        this.onShow()
+                    }
+                } else {
+                    return
                 }
             }
         },
@@ -86,9 +89,9 @@
     }
 </script>
 <style lang="scss" scoped>
-    $border-color:#333;
-    $border-radius:4px;
-    .popover{
+    $border-color: #333;
+    $border-radius: 4px;
+    .popover {
         /*如果不加这个默认是行级元素，再写一个button就会占两行，我们只要一行*/
         display: inline-block;
         /*行内对齐方式*/
@@ -96,11 +99,11 @@
         position: relative;
     }
 
-    .content-wrapper{
+    .content-wrapper {
         position: absolute;
         border: 1px solid $border-color;
         border-radius: $border-radius;
-        filter: drop-shadow(0 1px 3px rgba(0,0,0,0.5));
+        filter: drop-shadow(0 1px 3px rgba(0, 0, 0, 0.5));
         background: white;
         /*使组件出现的relatetive组件的上方，用就行了*/
         /*给个padding*/
@@ -110,13 +113,14 @@
         word-break: break-all;
         max-width: 20em;
     }
-    .content-wrapper.position-top{
+
+    .content-wrapper.position-top {
         transform: translateY(-100%);
         margin: -10px 0px;
     }
 
     /*画一个三角形*/
-    .content-wrapper.position-top::before{
+    .content-wrapper.position-top::before {
         content: '';
         display: block;
         border: 10px solid transparent;
@@ -128,7 +132,7 @@
         left: 8px;
     }
 
-    .content-wrapper.position-top::after{
+    .content-wrapper.position-top::after {
         content: '';
         display: block;
         border: 10px solid transparent;
@@ -141,11 +145,12 @@
         left: 8px;
     }
 
-    .content-wrapper.position-bottom{
+    .content-wrapper.position-bottom {
         transform: translateY(100%);
         margin: 10px 0px;
     }
-    .content-wrapper.position-bottom::before{
+
+    .content-wrapper.position-bottom::before {
         content: '';
         display: block;
         border: 10px solid transparent;
@@ -157,7 +162,7 @@
         left: 8px;
     }
 
-    .content-wrapper.position-bottom::after{
+    .content-wrapper.position-bottom::after {
         content: '';
         display: block;
         border: 10px solid transparent;
@@ -171,12 +176,12 @@
     }
 
 
-    .content-wrapper.position-left{
+    .content-wrapper.position-left {
         transform: translateX(-100%);
         margin-left: -10px;
     }
 
-    .content-wrapper.position-left::before{
+    .content-wrapper.position-left::before {
         content: '';
         display: block;
         border: 10px solid transparent;
@@ -189,7 +194,7 @@
         transform: translateY(-50%);
     }
 
-    .content-wrapper.position-left::after{
+    .content-wrapper.position-left::after {
         content: '';
         display: block;
         border: 10px solid transparent;
@@ -204,12 +209,12 @@
     }
 
 
-    .content-wrapper.position-right{
+    .content-wrapper.position-right {
         transform: translateX(100%);
         margin-left: 10px;
     }
 
-    .content-wrapper.position-right::before{
+    .content-wrapper.position-right::before {
         content: '';
         display: block;
         border: 10px solid transparent;
@@ -222,7 +227,7 @@
         transform: translateY(-50%);
     }
 
-    .content-wrapper.position-right::after{
+    .content-wrapper.position-right::after {
         content: '';
         display: block;
         border: 10px solid transparent;

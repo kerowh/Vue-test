9a6086b246f65f7d994f1f915ce9036f9c773ee0
keo <380213949@qq.com>
2019-07-04 10:57:31 +0800

    添加默认样式

--- src/popover.vue
+++ src/popover.vue
@@ -4,7 +4,7 @@
         <div ref="contentWrapper" class="content-wrapper" v-if="visible">
             <slot name="content" ></slot>
         </div>
-        <span ref="triggerWrapper">
+        <span ref="triggerWrapper" style="display: inline-block">
             <slot></slot>
         </span>
     </div>
@@ -26,7 +26,6 @@
             listenToDocument(){
                 let onClickDocunment = (event)=>{
                     /*如果它存在并且内容一致才什么都不做*/
-                    //
                     if (this.$refs.contentWrapper && this.$refs.contentWrapper.contains(event.target)){
                         return
                     }else {
@@ -52,21 +51,6 @@
                 }else {
                    return
                 }
-
-                // this.visible = !this.visible
-                // if (this.visible === true){
-                //     setTimeout(()=>{
-                //         document.body.appendChild(this.$refs.contentWrapper)
-                //         let{left,top}=this.$refs.triggerWrapper.getBoundingClientRect()
-                //         this.$refs.contentWrapper.style.left=left+window.scrollX+'px'
-                //         this.$refs.contentWrapper.style.top=top+window.scrollY+'px'
-                //         let EventHandle = ()=>{
-                //             this.visible = false
-                //             document.removeEventListener('click',EventHandle)
-                //         }
-                //         document.addEventListener('click',EventHandle)
-                //     },0)
-                // }
             }
         },
         mounted() {
@@ -75,6 +59,8 @@
     }
 </script>
 <style lang="scss" scoped>
+    $border-color:#333;
+    $border-radius:4px;
     .popover{
         /*如果不加这个默认是行级元素，再写一个button就会占两行，我们只要一行*/
         display: inline-block;
@@ -85,8 +71,46 @@
 
     .content-wrapper{
         position: absolute;
-        border: 1px solid red;
-        box-shadow: 0 0 3px rgba(0,0,0,0.5);
+        border: 1px solid $border-color;
+        border-radius: $border-radius;
+        /*box-shadow: 0 0 3px rgba(0,0,0,0.5);*/
+        filter: drop-shadow(0 1px 3px rgba(0,0,0,0.5));
+        background: white;
+        /*使组件出现的relatetive组件的上方，用就行了*/
         transform: translateY(-100%);
+        /*给个padding*/
+        padding: .5em 1em;
+        /*上移10个像素*/
+        margin: -10px 0px;
+        /*写了最大宽度了，就需要换行，中文不需要*/
+        word-break: break-all;
+        max-width: 20em;
+
+    }
+
+    /*画一个三角形*/
+    .content-wrapper::before{
+        content: '';
+        display: block;
+        border: 10px solid transparent;
+        border-top-color: black;
+        width: 0px;
+        height: 0px;
+        position: absolute;
+        top: 100%;
+        left: 8px;
+    }
+
+    .content-wrapper::after{
+        content: '';
+        display: block;
+        border: 10px solid transparent;
+        border-top-color: white;
+        width: 0px;
+        height: 0px;
+        position: absolute;
+        /*向上抬1.5像素*/
+        top: calc(100% - 1.5px);
+        left: 8px;
     }
 </style>

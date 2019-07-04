6a28d69780677e7f13b68ea61de3768ce5e705cb
keo <380213949@qq.com>
2019-07-04 20:34:42 +0800

    解决抖动bug

--- src/popover.vue
+++ src/popover.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="popover" @click="onClick">
+    <div class="popover" ref="popover">
         <!--        因为slot不能加class，没法写css-->
         <div ref="contentWrapper" class="content-wrapper" v-if="visible"
              :class="{[`position-${position}`]:true}">
@@ -21,10 +21,49 @@
                 validator(value) {
                     return ['top', 'bottom', 'left', 'right'].indexOf(value) >= 0
                 }
+            },
+            trigger:{
+                type:String,
+                default: 'click',
+                validator(value){
+                    return ['click','hover'].indexOf(value)>=0
+                }
             }
         },
         data() {
             return {visible: false}
+        },
+        mounted(){
+          if (this.trigger ==='click'){
+              this.$refs.popover.addEventListener('click',this.onClick)
+          }else {
+              this.$refs.popover.addEventListener('mouseenter',this.open)
+              this.$refs.popover.addEventListener('mouseleave',this.close)
+          }
+        },
+        destroyed(){
+            if (this.trigger ==='click'){
+                this.$refs.popover.removeEventListener('click',this.onClick)
+            }else {
+                this.$refs.popover.removeEventListener('mouseenter',this.open)
+                this.$refs.popover.removeEventListener('mouseleave',this.close)
+            }
+        },
+        computed:{
+            // openEvent(){
+            //     if (this.trigger==='click'){
+            //         return 'click'
+            //     } else {
+            //         return 'mouseenter'
+            //     }
+            // },
+            // closeEvent(){
+            //     if (this.trigger === 'click'){
+            //         return 'click'
+            //     }else {
+            //         return 'mouseleave'
+            //     }
+            // }
         },
         methods: {
             positionContent() {
@@ -53,39 +92,39 @@
                 contentWrapper.style.top = positions[this.position].top  + 'px'
                 contentWrapper.style.left = positions[this.position].left + 'px'
             },
-            listenToDocument() {
-                let onClickDocunment = (event) => {
-                    /*如果它存在并且内容一致才什么都不做*/
-                    if (this.$refs.contentWrapper && this.$refs.contentWrapper.contains(event.target)) {
-                        return
-                    } else {
-                        this.visible = false
-                        document.removeEventListener('click', onClickDocunment)
-                    }
-                }
-                document.addEventListener('click', onClickDocunment)
+            onClickDocunment(event){
+                /*如果它存在并且内容一致才什么都不做*/
+                if (this.$refs.contentWrapper &&
+                    this.$refs.contentWrapper.contains(event.target))
+                {return}
+                this.close()
             },
-            onShow() {
+            listenToDocument(){
+                document.addEventListener('click', this.onClickDocunment)
+            },
+            open() {
+                this.visible = true
                 setTimeout(() => {
                     this.positionContent()
                     this.listenToDocument()
                 }, 0)
             },
+            close(){
+              this.visible = false
+              document.removeEventListener('click', this.onClickDocunment)
+            },
+
             onClick(event) {
                 if (this.$refs.triggerWrapper.contains(event.target)) {
                     // console.log('点的是按钮');
-                    this.visible = !this.visible
                     if (this.visible === true) {
-                        this.onShow()
+                        this.close()
+                    }else {
+                        this.open()
                     }
-                } else {
-                    return
                 }
             }
         },
-        mounted() {
-
-        }
     }
 </script>
 <style lang="scss" scoped>
@@ -125,6 +164,7 @@
         display: block;
         border: 10px solid transparent;
         border-top-color: black;
+        border-bottom: none;
         width: 0px;
         height: 0px;
         position: absolute;
@@ -136,6 +176,7 @@
         content: '';
         display: block;
         border: 10px solid transparent;
+        border-bottom: none;
         border-top-color: white;
         width: 0px;
         height: 0px;
@@ -154,6 +195,7 @@
         content: '';
         display: block;
         border: 10px solid transparent;
+        border-top: none;
         border-bottom-color: black;
         width: 0px;
         height: 0px;
@@ -166,6 +208,7 @@
         content: '';
         display: block;
         border: 10px solid transparent;
+        border-top: none;
         border-bottom-color: white;
         width: 0px;
         height: 0px;
@@ -185,6 +228,7 @@
         content: '';
         display: block;
         border: 10px solid transparent;
+        border-right: none;
         border-left-color: black;
         width: 0px;
         height: 0px;
@@ -198,6 +242,7 @@
         content: '';
         display: block;
         border: 10px solid transparent;
+        border-right: none;
         border-left-color: white;
         width: 0px;
         height: 0px;
@@ -218,6 +263,7 @@
         content: '';
         display: block;
         border: 10px solid transparent;
+        border-left: none;
         border-right-color: black;
         width: 0px;
         height: 0px;
@@ -231,6 +277,7 @@
         content: '';
         display: block;
         border: 10px solid transparent;
+        border-left: none;
         border-right-color: white;
         width: 0px;
         height: 0px;
